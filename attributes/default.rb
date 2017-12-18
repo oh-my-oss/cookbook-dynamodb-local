8b1e285b1a88e459534a140b51c7a44405cf1280
Shinichi Urabe <urabe@nuts-choco.com>
2017-12-19 00:44:06 +0900

    update download_url

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@ default["dynamodb-local"] = {
   "user" => "dynamodb",
   "directory" => "/usr/local/lib/dynamodb-local",
   "log_dir" => "/var/log/dynamodb-local",
-  "download_url" => "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest",
+  "download_url" => "https://s3-ap-northeast-1.amazonaws.com/dynamodb-local-tokyo/dynamodb_local_latest.tar.gz",
   "port" => 8000
 }
